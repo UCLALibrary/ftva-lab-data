cc52bd747c21d04990a0b6eb478c98402d3261f5
Zoe Tucker <zoetucker@library.ucla.edu>
2025-05-21 16:42:43 -0700

    use context dictionary for add_edit_item views

--- ftva_lab_data/views.py
+++ ftva_lab_data/views.py
@@ -5,6 +5,13 @@ from .models import SheetImport
 
 
 def add_item(request):
+    # context values to be passed to the add_edit_item template
+    add_item_context = {
+        "form": ItemForm(),
+        "title": "Add Item",
+        "button_text": "Add Item",
+    }
+
     if request.method == "POST":
         # save a new SheetImport object
         form = ItemForm(request.POST)
@@ -12,28 +19,25 @@ def add_item(request):
             form.save()
             messages.success(request, "Item added successfully!")
             return redirect("view_item", item_id=form.instance.id)
-
         else:
             # Handle form errors, if needed
             messages.error(request, "Please correct the errors below.")
-            return render(
-                request,
-                "add_edit_item.html",
-                {"form": form, "title": "Add Item", "button_text": "Add Item"},
-            )
+            return render(request, "add_edit_item.html", add_item_context)
     else:
         # For GET requests, display the empty form
-        form = ItemForm()
-        return render(
-            request,
-            "add_edit_item.html",
-            {"form": form, "title": "Add Item", "button_text": "Add Item"},
-        )
+        return render(request, "add_edit_item.html", add_item_context)
 
 
 def edit_item(request, item_id):
     # Retrieve the item to edit
     item = SheetImport.objects.get(id=item_id)
+    # context values to be passed to the add_edit_item template
+    edit_item_context = {
+        "form": ItemForm(instance=item),
+        "item": item,
+        "title": "Edit Item",
+        "button_text": "Save Changes",
+    }
 
     if request.method == "POST":
         form = ItemForm(request.POST, instance=item)
@@ -43,29 +47,9 @@ def edit_item(request, item_id):
             return render(request, "view_item.html", {"item": item})
         else:
             messages.error(request, "Please correct the errors below.")
-            return render(
-                request,
-                "add_edit_item.html",
-                {
-                    "form": form,
-                    "item": item,
-                    "title": "Edit Item",
-                    "button_text": "Save Changes",
-                },
-            )
+            return render(request, "add_edit_item.html", edit_item_context)
     else:
-        form = ItemForm(instance=item)
-
-    return render(
-        request,
-        "add_edit_item.html",
-        {
-            "form": form,
-            "item": item,
-            "title": "Edit Item",
-            "button_text": "Save Changes",
-        },
-    )
+        return render(request, "add_edit_item.html", edit_item_context)
 
 
 def view_item(request, item_id):
