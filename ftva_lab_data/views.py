65d979aacf26dac0ef4f79644757636307b5d259
Zoe Tucker <zoetucker@library.ucla.edu>
2025-05-21 16:54:05 -0700

    SYS-1835: support for viewing, editing, and adding individual items (#3)
    * initial support for viewing, editing, and adding individual items
    * clean up old comments
    * remove add_item as default URL path
    * consistently use bootstrap_button in templates
    * fix formatting in base.html
    * combine add and edit templates
    * use context dictionary for add_edit_item views

--- ftva_lab_data/views.py
+++ ftva_lab_data/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ItemForm
+from .models import SheetImport
 
-# Create your views here.
+
+def add_item(request):
+    # context values to be passed to the add_edit_item template
+    add_item_context = {
+        "form": ItemForm(),
+        "title": "Add Item",
+        "button_text": "Add Item",
+    }
+
+    if request.method == "POST":
+        # save a new SheetImport object
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Item added successfully!")
+            return redirect("view_item", item_id=form.instance.id)
+        else:
+            # Handle form errors, if needed
+            messages.error(request, "Please correct the errors below.")
+            return render(request, "add_edit_item.html", add_item_context)
+    else:
+        # For GET requests, display the empty form
+        return render(request, "add_edit_item.html", add_item_context)
+
+
+def edit_item(request, item_id):
+    # Retrieve the item to edit
+    item = SheetImport.objects.get(id=item_id)
+    # context values to be passed to the add_edit_item template
+    edit_item_context = {
+        "form": ItemForm(instance=item),
+        "item": item,
+        "title": "Edit Item",
+        "button_text": "Save Changes",
+    }
+
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Item updated successfully!")
+            return render(request, "view_item.html", {"item": item})
+        else:
+            messages.error(request, "Please correct the errors below.")
+            return render(request, "add_edit_item.html", edit_item_context)
+    else:
+        return render(request, "add_edit_item.html", edit_item_context)
+
+
+def view_item(request, item_id):
+    # Retrieve the item to view
+    item = SheetImport.objects.get(id=item_id)
+
+    return render(request, "view_item.html", {"item": item})
