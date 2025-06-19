a535b8d89ddf0a7b33f68f36f91c456bca0b8e9e
Andy Kohler <akohler@library.ucla.edu>
2025-06-18 20:01:49 -0800

    Modify convert_dl_sheet_data to handle Hearst data

--- ftva_lab_data/management/commands/convert_dl_sheet_data.py
+++ ftva_lab_data/management/commands/convert_dl_sheet_data.py
@@ -5,32 +5,63 @@ from ftva_lab_data.models import SheetImport
 
 
 class Command(BaseCommand):
-    help = "Import Digital Labs Google Sheet data"
+    help = "Convert Digital Labs Google Sheet (Excel) data to a JSON fixture."
 
-    def _get_field_names(self) -> list[str]:
-        """Returns a list of the field names for the SheetImport model.  Field names
-        are in the same order they're defined on the model, which is the same order they
-        occur in the Google sheet.
+    def _get_field_names(self, sheet_id: str) -> list[str]:
+        """Returns a list of the `SheetImport` model fields expected for data coming from
+        `sheet_id`.  These must be in the same order that the source data will be, as these
+        serve as the mapping for source data with inconsistent column headers.
+
+        :param sheet_id: The generic name of the data source to be mapped.
+        :return: A list of strings representing the relevant `SheetImport` field names.
         """
-        # Ignore model _state, id (part of model but not in source data), and hard_drive_barcode_id
-        # (which will be handled as a special case when reading the data).
-        # Also ignore new field assigned_user_id.
-        # And new fields carrier_a_location and carrier_b_location; still easier to exclude
-        # fields than to list all 40+ to include by name.
-        field_names = [
-            key
-            for key in SheetImport().__dict__
-            if key
-            not in [
+
+        field_names = []
+        if sheet_id == "main_sheet":
+            # Get all field names from SheetImport model, except for a few specific ones
+            # which are not in the source data.  This is slightly fragile, as adding new
+            # fields to SheetImport will affect this; I added a comment to the model definition
+            # warning that excluded_fields must be updated if fields are added to the model.
+            # I still prefer this approach to explicitly naming all 40+ fields here.
+            excluded_fields = [
                 "_state",
                 "id",
-                "hard_drive_barcode_id",
+                "hard_drive_barcode_id",  # special case handled when processing the data.
                 "assigned_user_id",
                 "carrier_a_location",
                 "carrier_b_location",
             ]
-        ]
+
+            field_names = [
+                key for key in SheetImport().__dict__ if key not in excluded_fields
+            ]
+
+        elif sheet_id == "hearst_sheet":
+            # Only 7 fields in this source data (only 5 relevant ones, as the final 2
+            # (NetApp and TruNas)) will not be imported.
+            # The fields are in a different order, so be explicit here.
+            field_names = [
+                "carrier_a",
+                "carrier_a_location",
+                "file_folder_name",
+                "file_name",
+                "notes",
+            ]
+
         return field_names
+
+    def _get_sheet_data(self, file_name: str, sheet_name: str) -> list:
+        """Reads data from a sheet in an Excel file.
+
+        :param file_name: The name of the Excel file.
+        :param sheet_name: The name of the sheet within the Excel file.
+        :return rows: A list of rows, each containing the value.
+
+        """
+        df = pd.read_excel(file_name, sheet_name=sheet_name, dtype="string").fillna("")
+        rows = df.values.tolist()
+        self.stdout.write(f"Read {len(rows)} rows from {file_name} / {sheet_name}")
+        return rows
 
     def add_arguments(self, parser) -> None:
         parser.add_argument(
@@ -38,50 +69,43 @@ class Command(BaseCommand):
             "--file_name",
             type=str,
             required=True,
-            help="Name of the Excel file with data",
-        )
-        parser.add_argument(
-            "-s",
-            "--sheet_name",
-            type=str,
-            required=True,
-            help="Name of the sheet to import",
+            help="Name of the Excel file with DL data",
         )
 
     def handle(self, *args, **options) -> None:
         file_name = options["file_name"]
-        sheet_name = options["sheet_name"]
-        df = pd.read_excel(file_name, sheet_name=sheet_name, dtype="string").fillna("")
-        rows = df.values.tolist()
-        self.stdout.write(f"Read {len(rows)} rows from {file_name} / {sheet_name}")
+        # Process all specified sheets together, always;
+        # sheet names needed to read data from Excel are mapped here
+        # to generic identifiers used elsewhere in the program.
+        sheet_names = {
+            "main_sheet": "LTO-Backup",
+            "hearst_sheet": "Hearst ML Tapes",
+        }
 
         records = []
-        field_names = self._get_field_names()
-        # Loop through the rows of data, creating an object for all of the relevant ones.
-        for row_number, row in enumerate(rows, start=1):
-            # Actually, *don't* ignore pseudo-header rows, which are repeated in the source document
-            # and are inconsistent.  These can be identified by a specific value in column D,
-            # which is always the same. (Column A header is not completely consistent....).
-            # These will be used in a later cleanup process to identify where hard drives stop
-            # and tapes begin.
-            # if row[3] == "File Folder Name":
-            #     continue
+        for sheet_id, sheet_name in sheet_names.items():
+            rows = self._get_sheet_data(file_name, sheet_name)
+            field_names = self._get_field_names(sheet_id)
 
-            # Copy all of the fields as-is, to start
-            fields = {
-                field_name: row[field_number]
-                for field_number, field_name in enumerate(field_names, start=0)
-            }
+            # Loop through the rows of data, creating an object for all of the relevant ones.
+            for _, row in enumerate(rows, start=1):
+                # Copy all of the fields as-is, except for stripping leading / trailing whitespace.
+                fields = {
+                    field_name: row[field_number].strip()
+                    for field_number, field_name in enumerate(field_names, start=0)
+                }
 
-            # Finally, put it in the format needed for a fixture to load later.
-            record = {
-                "model": "ftva_lab_data.sheetimport",
-                "pk": row_number,
-                "fields": fields,
-            }
-            records.append(record)
+                # Finally, put it in the format needed for a fixture to load later.
+                record = {
+                    "model": "ftva_lab_data.sheetimport",
+                    "fields": fields,
+                }
+                records.append(record)
 
-        self.stdout.write(f"Finished: processed {len(records)} records")
-
-        with open("sheet_data.json", "w") as f:
+        output_file = "sheet_data.json"
+        with open(output_file, "w") as f:
             json.dump(records, f, indent=2)
+
+        self.stdout.write(
+            f"Finished: Wrote {len(records)} records from all sheets to {output_file}."
+        )
