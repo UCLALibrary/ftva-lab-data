3f9cfb50ea89903365973b4c497aaf3f53d95be9
Andy Kohler <akohler@library.ucla.edu>
2025-06-29 19:21:23 -0800

    Fix 'Uncaught TypeError' when assigned-users-form not in page

--- static/js/main.js
+++ static/js/main.js
@@ -18,43 +18,6 @@ function clearSearchInput() {
   htmx.trigger(input, "clear");
 }
 
-document.getElementById("assigned-users-form").onsubmit = function (e) {
-  const checked = Array.from(
-    document.querySelectorAll(".row-checkbox:checked")
-  ).map((cb) => cb.value);
-  let input = this.querySelector('input[name="ids"]');
-  if (!input) {
-    input = document.createElement("input");
-    input.type = "hidden";
-    input.name = "ids";
-    this.appendChild(input);
-  }
-  input.value = checked.join(",");
-
-  // Add current filters as hidden inputs
-  const search = document.querySelector('input[name="search"]');
-  const search_column = document.querySelector('select[name="search_column"]');
-  const page = document.querySelector("#current-page")?.value;
-
-  function setOrUpdate(name, value) {
-    let el = this.querySelector(`input[name="${name}"]`);
-    if (!el) {
-      el = document.createElement("input");
-      el.type = "hidden";
-      el.name = name;
-      this.appendChild(el);
-    }
-    el.value = value || "";
-  }
-  setOrUpdate.call(this, "search", search ? search.value : "");
-  setOrUpdate.call(
-    this,
-    "search_column",
-    search_column ? search_column.value : ""
-  );
-  setOrUpdate.call(this, "page", page || "");
-};
-
 document.addEventListener("htmx:afterSwap", function (e) {
   const selectAll = document.getElementById("select-all-checkbox");
   if (selectAll) {
@@ -72,4 +35,47 @@ document.addEventListener("DOMContentLoaded", function () {
       e.preventDefault(); // Prevent form submission on Enter
     });
   }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  assignedUsersForm = document.getElementById("assigned-users-form");
+  if (assignedUsersForm) {
+    assignedUsersForm.addEventListener("submit", function () {
+      const checked = Array.from(
+        document.querySelectorAll(".row-checkbox:checked")
+      ).map((cb) => cb.value);
+      let input = this.querySelector('input[name="ids"]');
+      if (!input) {
+        input = document.createElement("input");
+        input.type = "hidden";
+        input.name = "ids";
+        this.appendChild(input);
+      }
+      input.value = checked.join(",");
+
+      // Add current filters as hidden inputs
+      const search = document.querySelector('input[name="search"]');
+      const search_column = document.querySelector('select[name="search_column"]');
+      const page = document.querySelector("#current-page")?.value;
+
+      function setOrUpdate(name, value) {
+        let el = this.querySelector(`input[name="${name}"]`);
+        if (!el) {
+          el = document.createElement("input");
+          el.type = "hidden";
+          el.name = name;
+          this.appendChild(el);
+        }
+        el.value = value || "";
+      }
+      setOrUpdate.call(this, "search", search ? search.value : "");
+      setOrUpdate.call(
+        this,
+        "search_column",
+        search_column ? search_column.value : ""
+      );
+      setOrUpdate.call(this, "page", page || "");
+    });
+  };
 });
