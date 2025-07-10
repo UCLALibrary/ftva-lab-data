23557d8e7b74f6350c5f386763997d98214326bb
Henry Rosen <herosen@library.ucla.edu>
2025-07-10 09:14:08 -0700

    Add search fields for `SheetImportAdmin` class
    * Use `register` decorator in `admin.py`

--- ftva_lab_data/admin.py
+++ ftva_lab_data/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 from .models import SheetImport, ItemStatus
 
-admin.site.register(SheetImport, SimpleHistoryAdmin)
-admin.site.register(ItemStatus)
 
-
+@admin.register(ItemStatus)
 class ItemStatusAdmin(admin.ModelAdmin):
-    list_display = ("status", "display_name")
-    search_fields = ("status", "display_name")
+    list_display = ("status",)
+    search_fields = ("status",)
+
+
+@admin.register(SheetImport)
+class SheetImportAdmin(SimpleHistoryAdmin):
+    search_fields = ("id",)
